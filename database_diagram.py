ba4bc3019608eea49b4ad1cb587abd42bbdde0bc
anelisiweSaao <anelisiwe@saao.ac.za>
2018-02-07 11:59:48 +0200

    eddited some content

--- database_diagram.py
+++ database_diagram.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 
 # page files and titles
 pages = {
@@ -16,20 +15,25 @@ pages = {
     'Rss':'Rss.html'
 
 }
-titles = ['Home', 'Proposal', 'Block', 'Salticam', 'SologEvent', 'Fault', 'Filedata', 'OcsStep', 'Hrs', 'Bvit', 'Rss']
-files = ['Home-page.html'] + ['files/' + filename for filename in os.listdir(os.getcwd()+"/files")]
+titles = ['Home', 'Proposal', 'Block', 'Salticam', 'Rss', 'Hrs', 'Bvit', 'SologEvent', 'Fault', 'Filedata']
+files = ['Home-page.html', 'styling.css'] + ['files/' + filename for filename in os.listdir(os.getcwd()+"/files")]
+
+outputPath=os.getcwd()+'/output'
+if os.path.exists(outputPath):
+    for file in os.listdir(outputPath):
+        os.remove(outputPath +'/' +file)
+else:
+    os.mkdir(outputPath)
 
 for path in files: # files in a directory
     filename = os.path.split(path)[1]
-    if filename.endswith('.html'): #files that ends with .html extension
+    if filename.endswith(('.html','.css')): #files that ends with .html extension
         with open(path, 'r') as file: #open files to read and write
             current_page =filename
-            if not os.path.exists(os.getcwd() + '/output'):
-                os.makedirs('output')
-            with open('output/'+current_page ,'w')as newFiles:  # create new files
+            with open('output/' + current_page ,'w')as newFiles:  # create new files
                 for line in file: #loop every line in a file
                     if line == '<head>\n': # if line is head in html
-                        line +='<link rel = "stylesheet" type = "text/css" href = "../styling.css" >\n' #add this line below the head
+                        line +='<link rel = "stylesheet" type = "text/css" href = "styling.css" >\n' #add this line below the head
                     if line == '<body>\n':  # if line is body in html
                         line += '<div class="topnav" id="myTopnav"> \n'
                         for title in titles:
@@ -40,8 +44,3 @@ for path in files: # files in a directory
                             line += '<a class="' + className + '" href="' + pages[title] + '">' + title + '</a>'
                         line += '</div>\n'
                     newFiles.write(line)  # add this to new files created
-
-
-
-
-
